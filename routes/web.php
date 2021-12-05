a32ed1732c8be90ec9a52f9096e940eb504bf3b5
Masahiro Fujioka <fujioka8700@gmail.com>
2021-12-05 18:23:49 +0900

    [add] Googleのアカウントでログイン可能にする

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Auth::routes();
+Route::prefix('login')->name('login.')->group(function () {
+    Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
+});
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
